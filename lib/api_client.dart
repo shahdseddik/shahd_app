b6791514dfd474b6b409010910231c00d84af59d
shahdseddik <030121102@std.izu.edu.tr>
2025-09-29 13:04:22 +0300

    Add Pretty Dio logger package

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,12 +1,22 @@
 import 'package:dio/dio.dart';
-
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 class ApiClient {
   final Dio _dio;
 
   ApiClient()
       : _dio = Dio(
           BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com'),
-        );
+        ) {
+    _dio.interceptors.add(
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        error: true,
+        compact: false,
+      ),
+    );
+  }
 
   Dio get dio => _dio;
 }
